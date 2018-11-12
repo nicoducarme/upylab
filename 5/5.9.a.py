a03c631cacddc61c49f9b142242c47f164a04d67
nicoducarme <44968535+nicoducarme@users.noreply.github.com>
2018-11-12 11:53:21 +0100

    Update 5.9.a.py
    Auteur : Romain Bervoets

--- 5/5.9.a.py
+++ 5/5.9.a.py
@@ -1,26 +1,10 @@
+""" Author = Romain Bervoets"""
 def bornes(nombres):
-    long = len(nombres)
-    if long != 0 and long != 1:
-        n1 = nombres[0]
-        n2 = nombres[1]
-        for i in range(long-1):
-            if n1 < n2:
-                min = n1
-            else:
-                min = n2
-            n1 = min
-            if i != long-2:
-                n2 = nombres[i+2]
-        n1 = nombres[0]
-        n2 = nombres[1]
-        for j in range(long-1):
-            if n1 > n2:
-                max = n1
-            else:
-                max = n2
-            n1 = max
-            if j != long-2:
-                n2 = nombres[j+2]
-    elif long == 1:
-        min, max = 0,0
-    return (min,max)
+   max = nombres[0]
+   min = nombres[0]
+   for i in nombres:
+       if i > max:
+           max = i
+       if i < min:
+           min = i
+   return(min, max)
